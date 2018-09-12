db91d4d4d1852bd75e078cd0c8dd6b6f8d6c9c8e
Sander Koenders <sanderkoenders@gmail.com>
2018-09-12 17:15:13 +0200

    test(knkvApi): add tests for knkv api

--- src/services/matchApi/knkvApi.ts
+++ src/services/matchApi/knkvApi.ts
@@ -1,12 +1,36 @@
 import MatchApi from './matchApi';
 import { Match } from '../../entities';
+import fetch from 'node-fetch';
 
 export default class KnkvApi implements MatchApi {
-  public constructor(
-    private apiKey: string
-  ) { }
+  private static API_URL = 'https://www2.knkv.nl/kcp';
+  private apiKey: string;
 
-  public getMatchesForTeam(teamId: number): Promise<Match[]> {
-    throw new Error('Method not implemented.');
+  public constructor(apiKey: string) {
+    this.apiKey = apiKey;
+  }
+
+  public async getMatchesForTeam(teamId: number): Promise<Match[]> {
+    await this.fetchData(teamId);
+
+    return Promise.resolve([]);
+  }
+
+  private async fetchData(teamId: number) {
+    const response = await fetch(this.createRequestString(teamId));
+
+    return response;
+  }
+
+  private createRequestString(teamId: number) {
+    let requestString = KnkvApi.API_URL;
+
+    requestString += `/${this.apiKey}`;
+    requestString += `?callback=jsonp_return`;
+    requestString += `&team_id=${teamId}`;
+    requestString += `full=1`;
+    requestString += `f=get_program`;
+
+    return requestString;
   }
 }
