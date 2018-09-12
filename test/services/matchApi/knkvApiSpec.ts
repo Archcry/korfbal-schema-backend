e3edd2fb637c628c9fc7f8472128c02c0c89f57e
Sander Koenders <sanderkoenders@gmail.com>
2018-09-12 07:16:36 +0200

    test(knkApi): specs for knknv api

---
+++ test/services/matchApi/knkvApiSpec.ts
@@ -0,0 +1,7 @@
+describe('Knkv Api', () => {
+  it('should retrieve matches and convert them to a certain format');
+
+  it('should throw an error when something went wrong when fetching matches');
+
+  it('should filter matches with no relevance to the given teamid');
+});
