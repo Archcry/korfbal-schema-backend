4f7a07cdc6cce67d49d438cc0d3dc873bff6702a
Sander Koenders <sanderkoenders@gmail.com>
2018-09-16 20:45:39 +0200

    feat(DriveAndWash): Add entity for DriveAndWash

--- src/entities.ts
+++ src/entities.ts
@@ -1,5 +1,9 @@
 import { Moment } from 'moment';
 
+export interface CombinedMatchInfo extends Match, WashAndDrive {
+  // Combines Match, WashAndDrive and Location (in the future)
+}
+
 export interface Match {
   id: number;
   homeTeam: Team;
@@ -18,4 +22,9 @@ export interface Facility {
   address: string;
   zipCode: string;
   city: string;
+}
+
+export interface WashAndDrive {
+  drive: string[];
+  wash: string[];
 }
