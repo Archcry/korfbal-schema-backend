50842b3ccd4e9786e5b159fbbbd83814386ae3ef
Sander Koenders <sanderkoenders@gmail.com>
2018-09-17 19:53:40 +0200

    refactor(entities): add entities for MatchApi in MatchApi interface

--- src/services/matchApi/matchApi.ts
+++ src/services/matchApi/matchApi.ts
@@ -1,5 +1,25 @@
-import { Match } from '../../entities';
+import { Moment } from 'moment';
 
 export default interface MatchApi {
-  getMatchesForTeam(teamId: number): Promise<Match[]>;
+  getMatchesForTeam(teamId: number): Promise<MatchApiResponseEntry[]>;
+}
+
+export interface MatchApiResponseEntry {
+  id: number;
+  homeTeam: Team;
+  awayTeam: Team;
+  dateTime: Moment;
+  facility: Facility;
+}
+
+export interface Team {
+  id: number;
+  name: string;
+}
+
+export interface Facility {
+  name: string;
+  address: string;
+  zipCode: string;
+  city: string;
 }
