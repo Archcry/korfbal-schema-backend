bd27b31a75a138ed1777bb9afda37d6524b98702
Sander Koenders <sanderkoenders@gmail.com>
2018-09-18 19:18:57 +0200

    feat(endpoint): add api endpoint for matches

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import * as path from 'path';
 import TYPES from './constants/types';
 import DutySchemaApi from './services/dutySchemaApi/dutySchemeApi';
 import FsDutySchemaApi from './services/dutySchemaApi/fsDutySchemaApi';
+import MatchInfoProvider from './providers/matchInfoProvider';
+import KnkvApi from './services/matchApi/knkvApi';
+import MatchApi from './services/matchApi/matchApi';
 
 class Main {
   private app: express.Application;
@@ -28,13 +31,22 @@ class Main {
     container.bind<Environment>(TYPES.Environment).toConstantValue(this.environment);
     container.bind(TYPES.Console).toConstantValue(global.console);
 
+    // Initialize match api and add it to the ioc container
+    container.bind<MatchApi>(TYPES.MatchApi).toConstantValue(new KnkvApi(this.environment.knkvApiKey));
+
     // Initialize duty schema api and add it to the ioc container
     const dutySchemaLocation = path.resolve(this.environment.projectRoot, this.environment.dutySchemaLocation);
     container.bind<DutySchemaApi>(TYPES.DutySchemaApi).toConstantValue(new FsDutySchemaApi(dutySchemaLocation));
   }
 
   public onListening() {
-    // To be implemented
+    const matchProvider = this.iocContext.getContainer().get<MatchInfoProvider>(TYPES.MatchProvider);
+
+    this.app.get('/matches', async (request, response) => {
+      if (request) {
+        response.send(await matchProvider.getMatchesForTeam(this.environment.teamId));
+      }
+    });
   }
 
   public get App(): express.Application {
