297c31a92124b139a26fe056968bbfd6eff0c122
Sander Koenders <sanderkoenders@gmail.com>
2018-09-18 16:25:21 +0200

    feat(MatchInfoProvider): combine information for matches and dutySchema

--- src/providers/matchInfoProvider.ts
+++ src/providers/matchInfoProvider.ts
@@ -1,13 +1,66 @@
 import { inject } from 'inversify';
 import TYPES from '../constants/types';
-import MatchApi from '../services/matchApi/matchApi';
+import MatchApi, { MatchApiResponseEntry } from '../services/matchApi/matchApi';
+import DutySchemaApi, { DutySchemaApiResponseEntry } from '../services/dutySchemaApi/dutySchemeApi';
+import { Moment } from 'moment';
 
 export default class MatchInfoProvider {
   constructor(
-    @inject(TYPES.MatchApi) private matchApi: MatchApi
+    @inject(TYPES.MatchApi) private matchApi: MatchApi,
+    @inject(TYPES.DutySchemaApi) private dutySchemaApi: DutySchemaApi
   ) { }
 
-  public async getMatchesForTeam(teamId: number) {
-    this.matchApi.getMatchesForTeam(teamId);
+  public async getMatchesForTeam(teamId: number): Promise<Match[]> {
+    const matchEntries = await this.matchApi.getMatchesForTeam(teamId);
+    const dutySchemaEntries = await this.dutySchemaApi.getDutySchemaEntries();
+
+    const matches = [];
+
+    for (const matchEntry of matchEntries) {
+      for (const dutySchemaEntry of dutySchemaEntries) {
+        if (matchEntry.id === dutySchemaEntry.gameId) {
+          matches.push(this.createMatch(matchEntry, dutySchemaEntry));
+        }
+      }
+    }
+
+    return matches;
   }
+
+  private createMatch(matchEntry: MatchApiResponseEntry, dutySchemaEntry: DutySchemaApiResponseEntry): Match {
+    return {
+      id: matchEntry.id,
+      homeTeam: matchEntry.homeTeam,
+      awayTeam: matchEntry.awayTeam,
+      dateTime: matchEntry.dateTime,
+      facility: matchEntry.facility,
+      dutySchema: dutySchemaEntry
+    };
+  }
+}
+
+export interface Match {
+  id: number;
+  homeTeam: Team;
+  awayTeam: Team;
+  dateTime: Moment;
+  facility: Facility;
+  dutySchema: DutySchema;
+}
+
+export interface Team {
+  id: number;
+  name: string;
+}
+
+export interface Facility {
+  name: string;
+  address: string;
+  zipCode: string;
+  city: string;
+}
+
+export interface DutySchema {
+  wash: string[];
+  drive: string[];
 }
--- test/unit/providers/matchInfoProviderSpec.ts
+++ test/unit/providers/matchInfoProviderSpec.ts
@@ -1,14 +1,17 @@
 import 'reflect-metadata';
-import { assert } from 'chai';
+import { expect, assert } from 'chai';
 import * as sinon from 'sinon';
+import * as moment from 'moment';
 import MatchInfoProvider from '../../../src/providers/matchInfoProvider';
-import MatchApi from '../../../src/services/matchApi/matchApi';
+import MatchApi, { MatchApiResponseEntry } from '../../../src/services/matchApi/matchApi';
+import DutySchemaApi, { DutySchemaApiResponseEntry } from '../../../src/services/dutySchemaApi/dutySchemeApi';
 
 describe('MatchInfoProvider', () => {
   const sandbox = sinon.createSandbox();
   const teamId = 13879;
 
   let matchApiStub: sinon.SinonStubbedInstance<MatchApi>;
+  let dutySchemaApiStub: sinon.SinonStubbedInstance<DutySchemaApi>;
 
   let subjectUnderTest: MatchInfoProvider;
 
@@ -17,7 +20,14 @@ describe('MatchInfoProvider', () => {
       getMatchesForTeam: sandbox.stub()
     };
 
-    subjectUnderTest = new MatchInfoProvider(matchApiStub);
+    dutySchemaApiStub = {
+      getDutySchemaEntries: sandbox.stub()
+    };
+
+    matchApiStub.getMatchesForTeam.returns(Promise.resolve(matchApiFakeResponse));
+    dutySchemaApiStub.getDutySchemaEntries.returns(Promise.resolve(dutySchemaApiFakeResponse));
+
+    subjectUnderTest = new MatchInfoProvider(matchApiStub, dutySchemaApiStub);
   });
 
   afterEach(() => sandbox.restore());
@@ -28,7 +38,85 @@ describe('MatchInfoProvider', () => {
     assert(matchApiStub.getMatchesForTeam.calledWith(teamId), `matchApi not called with teamId: "${teamId}"`);
   });
 
-  it('should fetch dutySchema information using the dutySchemaApi');
+  it('should fetch dutySchema information using the dutySchemaApi', async () => {
+    await subjectUnderTest.getMatchesForTeam(teamId);
 
-  it('should combine all fetched data in an CombinedMatchInfo object');
+    assert(dutySchemaApiStub.getDutySchemaEntries.called, 'dutySchemaApi.getDutySchemaEntries should\'ve been called');
+  });
+
+  it('should combine all fetched data in an CombinedMatchInfo object', async () => {
+    const matches = await subjectUnderTest.getMatchesForTeam(teamId);
+
+    expect(matches).to.deep.equal([
+      {
+        id: 25740,
+        homeTeam: { id: 12723, name: 'Aladna/De Issel 1' },
+        awayTeam: { id: 13879, name: 'Zwaluwen 4' },
+        facility: { name: 'Sportpark Zuid', address: 'Bocholtsestraatweg 67 a', zipCode: '7121JB', city: 'Aalten' },
+        dateTime: moment('2018-09-16 13:00'),
+        dutySchema:   {
+          gameId: 25740,
+          drive: ['Carl', 'Jane'],
+          wash: ['Carl']
+        }
+      },
+      {
+        id: 22831,
+        homeTeam: { id: 44345, name: 'Diderna/Visser Sloopwerken 3' },
+        awayTeam: { id: 13879, name: 'Zwaluwen 4' },
+        facility: { name: 'Het Nieuwland', address: 'Kolonieweg 2', zipCode: '6952GX', city: 'Dieren' },
+        dateTime: moment('2018-10-21 13:00'),
+        dutySchema: {
+          gameId: 22831,
+          drive: ['Sarah', 'Wren'],
+          wash: ['Sarah']
+        }
+      }
+    ]);
+  });
+
+  it('should throw an error when the matchApi cannot resolve match entries');
+
+  it('should throw an error when the dutySchemaApi cannot resolve the dutySchema entries');
 });
+
+const matchApiFakeResponse: MatchApiResponseEntry[] = [
+  {
+    id: 25740,
+    homeTeam: { id: 12723, name: 'Aladna/De Issel 1' },
+    awayTeam: { id: 13879, name: 'Zwaluwen 4' },
+    facility: { name: 'Sportpark Zuid', address: 'Bocholtsestraatweg 67 a', zipCode: '7121JB', city: 'Aalten' },
+    dateTime: moment('2018-09-16 13:00')
+  },
+  {
+    id: 56498,
+    homeTeam: { id: 13879, name: 'Zwaluwen 4' },
+    awayTeam: { id: 13582, name: 'Olympus \'58 3' },
+    facility: { name: 'Sportpark Hengelder', address: 'Marconistraat 18', zipCode: '6902PC', city: 'Zevenaar' },
+    dateTime: moment('2018-10-14 13:00')
+  },
+  {
+    id: 22831,
+    homeTeam: { id: 44345, name: 'Diderna/Visser Sloopwerken 3' },
+    awayTeam: { id: 13879, name: 'Zwaluwen 4' },
+    facility: { name: 'Het Nieuwland', address: 'Kolonieweg 2', zipCode: '6952GX', city: 'Dieren' },
+    dateTime: moment('2018-10-21 13:00')
+  }
+];
+
+const dutySchemaApiFakeResponse: DutySchemaApiResponseEntry[] = [
+  {
+    gameId: 25740,
+    drive: ['Carl', 'Jane'],
+    wash: ['Carl']
+  }, {
+    gameId: 22831,
+    drive: ['Sarah', 'Wren'],
+    wash: ['Sarah']
+  },
+  {
+    gameId: 1234,
+    drive: ['Theo', 'Angela'],
+    wash: ['theo']
+  }
+];
