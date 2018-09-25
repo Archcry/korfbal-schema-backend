434bf80360528021774b79171ec72738bc4a4b0a
Sander Koenders <sanderkoenders@gmail.com>
2018-09-25 08:36:54 +0200

    feat(TravelApi): Partial implementation for TravelApi.

--- src/services/travelApi/googleDistanceMatrixApi.ts
+++ src/services/travelApi/googleDistanceMatrixApi.ts
@@ -1,7 +1,31 @@
 import TravelApi, { TravelApiResponseEntry, Location } from './travelApi';
+import fetch from 'node-fetch';
 
 export default class GoogleDistanceMatrixApi implements TravelApi {
-  public getDistance(from: Location, to: Location, arrivalTime: number): Promise<TravelApiResponseEntry> {
-    throw new Error('Method not implemented.' + from + to + arrivalTime);
+  private static API_URL = 'https://maps.googleapis.com/maps/api/distancematrix/json';
+
+  public constructor(
+    private apiKey: string
+  ) {}
+
+  public getTravelInfo(from: Location, to: Location, arrivalTime: number): Promise<TravelApiResponseEntry> {
+    const fromString = this.createStringLocation(from);
+    const destinationString = this.createStringLocation(to);
+
+    fetch(this.createRequestUrl(fromString, destinationString));
+    fetch(this.createRequestUrl(fromString, destinationString));
+
+    return Promise.resolve((from.address + to.address + arrivalTime) as any);
+  }
+
+  private createStringLocation(location: Location) {
+    return `${location.address}, ${location.zipCode} ${location.city}, ${location.country}`;
+  }
+
+  private createRequestUrl(originString: string, destinationString: string) {
+    return `${GoogleDistanceMatrixApi.API_URL}
+      ?key=${this.apiKey}
+      &origins=${originString}
+      &destinations=${destinationString}`;
   }
 }
